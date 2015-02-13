3f7f28b8435f97dc665eeeb66e51da872492a64f
mizugaki <takeshi.mizugaki@ast-net.co.jp>
2015-02-13 18:23:01 +0900

    Initial Commit

---
+++ sampleBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleBrowser
+//
+//  Created by Mizugaki on 2015/02/13.
+//  Copyright (c) 2015年 mzgk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sampleBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleBrowser
+//
+//  Created by Mizugaki on 2015/02/13.
+//  Copyright (c) 2015年 mzgk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
